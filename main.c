e32d83ed72016a4c00e4ba71897c37e1d6c44473
root <root@Jonathan-Laptop.localdomain>
2020-07-22 19:47:31 +0200

    fix multiple issues

--- main.c
+++ main.c
@@ -1,187 +1,194 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
-char *token;
+#define DEBUG 0
+#define debug(fmt, ...) \
+            do { if (DEBUG) printf(fmt, ##__VA_ARGS__); } while (0)
+
+char *code;
 char *input;
 int tok_ptr = 0;
 int line_counter = 1;
-int line_position = 1;
+int line_position = 0;
 int input_ptr = 0;
 int bracket_counter = 0;
 
-int bracket_stack[1000];
-int *last_bracket;
+int *brackets;
+int *test;
 int puffer;
 
-#define push(last_bracket, n) (*((last_bracket)++) = (n))
-#define pop(last_bracket) (*--(last_bracket))
+unsigned char *registers;
+unsigned char *r;
 
-char *registers;
-char *r;
-
-void delete_char(int i) {
-    int len = strlen(token);
-
-    for (; i < len - 1 ; i++)
-    {
-       token[i] = token[i+1];
-    }
-
-    token[i] = '\0';
+void push_bracket(int position) {
+    *(++brackets) = position;
 }
 
-char getToken() {
-    char c = token[tok_ptr++];
-    line_position++;
-    if (c == '\0') {
-        return 'E';
+int pop_bracket() {
+    return *(brackets--);
+}
+
+void delete_char(int i)
+{
+    int len = strlen(code);
+
+    for (; i < len - 1; i++)
+    {
+        code[i] = code[i + 1];
     }
-    if (c == '\n') {
+
+    code[i] = '\0';
+}
+
+char getToken(int skip)
+{
+    char c = code[tok_ptr++];
+    line_position++;
+    if (c == '\n')
+    {
         line_counter++;
         line_position = 1;
     }
     return c;
 }
 
-int scanner() {
-    char cur_tok;
-    
-    while(cur_tok != 'E') {
-        cur_tok = getToken();
-        
-        switch (cur_tok) {
-            case '+':
-            case '-':
-            case '<':
-            case '>':
-            case '.':
-            case ',':
-                break;
-            case '[':
-                bracket_counter++;
-                break;
-            case ']':
-                if (bracket_counter == 0) {
-                    printf("No bracket to close in line %d at position %d\n", line_counter, tok_ptr);
-                    return 1;
-                }
-                bracket_counter--;
-                break;
-            case 'E':
-                if (bracket_counter != 0) {
-                    printf("Unclosed bracket\n");
-                    return 1;
-                }
-                break;
-            default:
-                delete_char(tok_ptr-1);
-                tok_ptr -= 1;
-                break;
-        }
-    }
-    return 0;
-}
-
-int interpret() {
-    last_bracket = bracket_stack;
-
+int interpret()
+{
+    brackets = calloc(200, sizeof(int));
+    test = brackets;
     registers = calloc(200, sizeof(char));
     r = registers;
-    registers += 100;   // Go to center of allocated memory
+    registers += 100; // Go to center of allocated memory
 
     tok_ptr = 0;
     char cur_tok;
-    int last_bracket_pos;
     int skip = 0;
+    
+    do
+    {
+        cur_tok = getToken(skip);
 
-    while(cur_tok != 'E') {
-        cur_tok = getToken();
-
-        if (skip == 1 && cur_tok == ']') {
-            skip = 0;
+        for(int i = 0; i < 5; i++) {
+            debug("%d\t", *(test + i));
         }
 
-        if (skip == 0) {
-            switch (cur_tok) {
-                case '+':
-                    (*registers)++;
-                    break;
-                case '-':
-                    (*registers)--;
-                    break;
-                case '<':
-                    *registers--;
-                    break;
-                case '>':
-                    *registers++;
-                    break;
-                case '.':
-                    printf("%c", *registers);
-                    break;
-                case ',':
-                    *registers = input[input_ptr++];
-                    break;
-                case '[':
-                    if (*registers != 0) {
-                        push(last_bracket, tok_ptr-1);
-                    } else {
-                        skip = 1;
-                    }
-                    break;
-                case ']':
-                    puffer = pop(last_bracket);
-                    if (*registers != 0) {
-                        tok_ptr = puffer;
-                    }
-                    break;
-                case 'E':
-                    printf("\n");
-                    break;
-                default:
-                    printf("Got different char in line %d at position %d: %c\n", line_counter, line_position, cur_tok);
-                    break;
+        debug("\n");
+        debug("LAST_BRACKET: %d\n", *brackets);
+
+        if (skip != 0) 
+        {
+            if(cur_tok == ']')
+            {
+                skip--;
+            }
+            else if (cur_tok == '[')
+            {
+                skip++;
             }
         }
-    }
+        else
+        {
+            debug("token: %c, position: %d, skip: %d\n", cur_tok, tok_ptr, skip);
+            switch (cur_tok)
+            {
+            case '+':
+                (*registers)++;
+                debug("Increased register at position %p to %d\n", registers, (int)*registers);
+                break;
+            case '-':
+                (*registers)--;
+                debug("Decreased register at position %p to %d\n", registers, (int)*registers);
+                break;
+            case '<':
+                *registers--;
+                debug("Switched to previous register\n");
+                break;
+            case '>':
+                *registers++;
+                debug("Switched to next register\n");
+                break;
+            case '.':
+                debug("Printing register at position %p. Value: %d\n", registers, (int)*registers);
+                printf("%c", *registers);
+                break;
+            case ',':
+                *registers = input[input_ptr++];
+                break;
+            case '[':
+                bracket_counter++;
+                if (*registers != 0)
+                {
+                    debug("pushing bracket at position %d\n", tok_ptr);
+                    push_bracket(tok_ptr);
+                }
+                else
+                {
+                    skip = 1;
+                }
+                break;
+            case ']':
+                bracket_counter--;
+                puffer = pop_bracket();
+                if (*registers != 0)
+                {
+                    debug("returning to last bracket at position %d\n", puffer);
+                    tok_ptr = puffer;
+                    tok_ptr--;
+                }
+                break;
+            case '\0':
+                if (bracket_counter != 0)
+                {
+                    debug("ERROR: Unclosed bracket\n");
+                    return 1;
+                }
+                printf("\n");
+                break;
+            default:
+                /* printf("Got different char in line %d at position %d: %c\n", line_counter, line_position, cur_tok); */
+                break;
+            }
+        }
+    } while (cur_tok != '\0');
 
     free(r);
     return 0;
 }
 
-void showHelp() {
+void showHelp()
+{
     printf("Usage: compiler \"[brainfuck_code]\" \"[input]\"\n");
 }
 
-int main(int argc, char* argv[]) {
-    if (argc == 1 || strcmp(argv[1], "--help") == 0) {
+int main(int argc, char *argv[])
+{
+    if (argc == 1 || strcmp(argv[1], "--help") == 0)
+    {
         showHelp();
         return 0;
     }
 
     struct stat buffer;
-    if (stat(argv[1], &buffer) == 0 ) {
+    if (stat(argv[1], &buffer) == 0)
+    {
         FILE *file = fopen(argv[1], "r");
         fseek(file, 0, SEEK_END);
         long fsize = ftell(file);
         fseek(file, 0, SEEK_SET);
-        token = malloc(fsize + 1);
-        fread(token, 1, fsize, file);
+        code = malloc(fsize + 1);
+        fread(code, 1, fsize, file);
         fclose(file);
-    } else {
-        token = argv[1];
+    }
+    else
+    {
+        code = argv[1];
     }
 
     input = argv[2];
 
-    if (scanner() != 0) {
-        return 1;
-    }
+    debug("%s\n", code);  
 
-    if (interpret() != 0) {
-        return 1;
-    }
-
-    return 0;
+    return interpret();
 }
